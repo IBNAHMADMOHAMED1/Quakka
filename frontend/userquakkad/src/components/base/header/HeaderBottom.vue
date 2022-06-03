ba010b9335794447b5c931551eaec573196039cc
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-06-03 23:53:04 +0000

    rap

--- frontend/userquakkad/src/components/base/header/HeaderBottom.vue
+++ frontend/userquakkad/src/components/base/header/HeaderBottom.vue
@@ -8,31 +8,15 @@
                         <div class="mega-category-menu">
                             <span class="cat-button"><i class="lni lni-menu"></i>All Categories</span>
                             <ul class="sub-category">
-                                <li><a href="product-grids.html">Electronics <i class="lni lni-chevron-right"></i></a>
-                                    <ul class="inner-sub-category">
-                                        <li><a href="product-grids.html">Digital Cameras</a></li>
-                                        <li><a href="product-grids.html">Camcorders</a></li>
-                                        <li><a href="product-grids.html">Camera Drones</a></li>
-                                        <li><a href="product-grids.html">Smart Watches</a></li>
-                                        <li><a href="product-grids.html">Headphones</a></li>
-                                        <li><a href="product-grids.html">MP3 Players</a></li>
-                                        <li><a href="product-grids.html">Microphones</a></li>
-                                        <li><a href="product-grids.html">Chargers</a></li>
-                                        <li><a href="product-grids.html">Batteries</a></li>
-                                        <li><a href="product-grids.html">Cables & Adapters</a></li>
-                                    </ul>
+                               <li @click="getCategory(1)">
+                               ANIMATION
+                               </li>
+                                <li @click="getCategory(2)">
+                                ACC
                                 </li>
-                                <li><a href="product-grids.html">accessories</a></li>
-                                <li><a href="product-grids.html">Televisions</a></li>
-                                <li><a href="product-grids.html">best selling</a></li>
-                                <li><a href="product-grids.html">top 100 offer</a></li>
-                                <li><a href="product-grids.html">sunglass</a></li>
-                                <li><a href="product-grids.html">watch</a></li>
-                                <li><a href="product-grids.html">man’s product</a></li>
-                                <li><a href="product-grids.html">Home Audio & Theater</a></li>
-                                <li><a href="product-grids.html">Computers & Tablets </a></li>
-                                <li><a href="product-grids.html">Video Games </a></li>
-                                <li><a href="product-grids.html">Home Appliances </a></li>
+                                <li @click="getCategory(3)"></li>
+                               
+                               
                             </ul>
                         </div>
                         <!-- End Mega Category Menu -->
--- frontend/userquakkad/src/components/base/header/HeaderMiddle.vue
+++ frontend/userquakkad/src/components/base/header/HeaderMiddle.vue
@@ -5,7 +5,7 @@
                     <div class="col-lg-3 col-md-3 col-7">
                         <!-- Start Header Logo -->
                         <a class="navbar-brand" href="index.html">
-                            <img src="assets/images/logo/logo.svg" alt="Logo">
+                           Quakka
                         </a>
                         <!-- End Header Logo -->
                     </div>
